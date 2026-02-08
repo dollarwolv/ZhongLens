7f5f36d5c2c09fb4cadd001a5d4dbe0406bc6849
dollarwolv <dollarwolv@gmail.com>
2026-02-08 19:14:57 +0800

    Set up virtual environment for python backend with requirements.txt

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,13 +1,13 @@
 from paddleocr import PaddleOCR
 import cv2
-from matplotlib import pyplot as plt
 
 ocr = PaddleOCR(
     use_doc_orientation_classify=False,  # Disables document orientation classification model via this parameter
     use_doc_unwarping=False,  # Disables text image rectification model via this parameter
     use_textline_orientation=False,  # Disables text line orientation classification model via this parameter
 )
-image_path = "jia.jpg"
+
+image_path = "jia.jpeg"
 img = cv2.imread(image_path)
 
 # this is my comment
