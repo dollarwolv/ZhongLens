0ca1861dd7b3bf6bedd82d73f59a5b3cdbdcc37a
dollarwolv <dollarwolv@gmail.com>
2026-02-09 17:47:36 +0800

    Refactor createChild function to include boxWidth parameter and adjust letter spacing calculation for improved text positioning in overlay

--- extension/src/overlay.js
+++ extension/src/overlay.js
@@ -1,13 +1,62 @@
-function createChild(textContent, xStart, yStart, boxHeight) {
+function createChild(textContent, xStart, yStart, boxHeight, boxWidth) {
   const LETTER_SPACING_K = 0.18;
   const childText = document.createElement("span");
   childText.textContent = textContent;
-  childText.style.top = `${yStart}px`;
-  childText.style.left = `${xStart}px`;
-  childText.style.fontSize = `${boxHeight * 0.8}px`;
-  childText.style.letterSpacing = `${48 * LETTER_SPACING_K}px`;
+
+  const fontSize = boxHeight * 0.8;
+  const initialLetterSpacing = 0;
+
+  let styles = {
+    whiteSpace: "pre",
+    top: `${yStart}px`,
+    left: `${xStart}px`,
+    fontSize: `${fontSize}px`,
+    letterSpacing: `${initialLetterSpacing}px`,
+  };
+
+  Object.assign(childText.style, styles);
+  let { width: computedWidth, height: computedHeight } = measureText(
+    textContent,
+    styles,
+  );
+
+  let widthDiffAbs = Math.abs(boxWidth - computedWidth);
+  let widthDiff = boxWidth - computedWidth;
+  // let heightDiff = Math.abs(boxHeight - height)\
+
+  const textLength = textContent.length;
+  const nSpaces = textLength - 1;
+
+  const newLetterSpacing = widthDiff / nSpaces;
+
+  styles.letterSpacing = `${newLetterSpacing}px`;
+  childText.style.letterSpacing = `${newLetterSpacing}px`;
+
+  console.log("initial letter spacing: " + initialLetterSpacing);
+  console.log("applied letter spacing: " + childText.style.letterSpacing);
+  console.log("inital measured box width: " + computedWidth);
+  let { width: newWidth, height: newHeight } = measureText(textContent, styles);
+  console.log("new measured box width: " + newWidth);
+  console.log("OCR box width: " + boxWidth);
+  console.log(" ");
+
   const overlay = document.getElementById("subtitle-overlay-chinese-ocr");
   overlay.append(childText);
+}
+
+function measureText(text, styles = {}) {
+  const span = document.createElement("span");
+  span.textContent = text;
+  span.style.position = "absolute";
+  span.style.visibility = "hidden";
+  span.style.whiteSpace = "pre";
+
+  Object.assign(span.style, styles);
+  document.body.appendChild(span);
+  const rect = span.getBoundingClientRect();
+  document.body.removeChild(span);
+
+  return { width: rect.width, height: rect.height };
 }
 
 function createOverlay() {
@@ -55,7 +104,7 @@ function drawOCROverlay(result) {
     const text = texts[i];
     console.log(text);
     const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = bboxes[i];
-    createChild(text, x1, y1, y3 - y1);
+    createChild(text, x1, y1, y3 - y1, x2 - x1);
   }
 }
 
