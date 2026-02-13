83b99e5d9a74cb98288d3e11760a7b4f06079004
dollarwolv <dollarwolv@gmail.com>
2026-02-13 10:10:29 +0800

    install wxt and begin refactor

---
+++ wxt-extension/entrypoints/background.js
@@ -0,0 +1,122 @@
+export default defineBackground(() => {
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log("Service worker installed");
+  });
+
+  async function dataUrlToBlob(dataUrl) {
+    const res = await fetch(dataUrl);
+    return await res.blob();
+  }
+
+  async function downscaleDataUrlToViewport(
+    dataUrl,
+    targetW,
+    targetH,
+    downscaleFurther = true,
+    downscaleMaxDim = 800,
+  ) {
+    // create blob that can then be converted to a bitmap, which is required for chrome
+    // to be able to draw on a canvas
+    const blob = await dataUrlToBlob(dataUrl);
+    const bitmap = await createImageBitmap(blob);
+
+    let scalingFactor = 1;
+    let downscaledW = null;
+    let downscaledH = null;
+
+    if (downscaleFurther) {
+      const maxDimViewport = Math.max(targetW, targetH);
+
+      if (maxDimViewport >= downscaleMaxDim) {
+        scalingFactor = downscaleMaxDim / maxDimViewport;
+        downscaledW = targetW * scalingFactor;
+        downscaledH = targetH * scalingFactor;
+      }
+    }
+
+    // create a canvas of smaller size and a context (which is what will be drawn on).
+    const canvas = new OffscreenCanvas(
+      downscaledW ?? targetW,
+      downscaledH ?? targetH,
+    );
+    const ctx = canvas.getContext("2d", { willReadFrequently: false }); // willReadFrequently is a resource-saving measure.
+
+    // draw image on the new canvas to downscale
+    ctx.drawImage(bitmap, 0, 0, downscaledW ?? targetW, downscaledH ?? targetH);
+
+    // convert to grayscale for extra good compression
+    const imgData = ctx.getImageData(
+      0,
+      0,
+      downscaledW ?? targetW,
+      downscaledH ?? targetH,
+    );
+    const data = imgData.data;
+    for (let i = 0; i < data.length; i += 4) {
+      const gray = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
+      data[i] = data[i + 1] = data[i + 2] = gray;
+    }
+    ctx.putImageData(imgData, 0, 0);
+
+    // creates a blob from this downscaled image
+    const outBlob = await canvas.convertToBlob({
+      type: "image/jpeg",
+      quality: 0.92,
+    });
+
+    console.log(outBlob.size / 1024); // KB
+
+    // convert blob to URL
+    const outDataUrl = await new Promise((resolve) => {
+      const r = new FileReader();
+      r.onloadend = () => resolve(r.result);
+      r.readAsDataURL(outBlob);
+    });
+
+    return { outBlob, outDataUrl, scalingFactor };
+  }
+
+  chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.type !== "CAPTURE_TAB") {
+      return;
+    }
+    try {
+      (async () => {
+        // capture tab and save image Data URL
+        const dataUrl = await chrome.tabs.captureVisibleTab();
+
+        // downscale URL to CSS Viewport size
+        const { outBlob, scalingFactor } = await downscaleDataUrlToViewport(
+          dataUrl,
+          msg.cssW,
+          msg.cssH,
+        );
+
+        // create form to send data over to backend and attach outBlob
+        const form = new FormData();
+        form.append("raw_img", outBlob, "frame.jpeg");
+
+        const res = await fetch("http://127.0.0.1:8000/ocr", {
+          method: "POST",
+          body: form,
+        });
+
+        if (!res.ok) throw new Error(await res.text());
+        const result = await res.json();
+
+        console.log(result.result.res);
+        console.log("scaling factor: " + scalingFactor);
+
+        sendResponse({
+          ok: true,
+          result: result.result.res,
+          scalingFactor,
+        });
+      })();
+    } catch (err) {
+      sendResponse({ ok: false, error: String(err) });
+    }
+
+    return true;
+  });
+});
---
+++ wxt-extension/entrypoints/overlay.content/index.jsx
@@ -0,0 +1,38 @@
+import ReactDOM from "react-dom/client";
+import Overlay from "./Overlay";
+
+export default defineContentScript({
+  matches: ["<all_urls>"],
+  cssInjectionMode: "ui",
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: "subtitle-overlay-chinese-ocr",
+      position: "fixed",
+      anchor: "body",
+      onMount: (container) => {
+        const app = document.createElement("div");
+        container.append(app);
+
+        const root = ReactDOM.createRoot(app);
+        root.render(<Overlay />);
+        return root;
+      },
+      onRemove: (root) => {
+        root?.unmount();
+      },
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.ctrlKey && e.key.toLowerCase() === "b") {
+        e.preventDefault();
+        if (!ui.mounted) {
+          ui.mount();
+        }
+      }
+      if (e.ctrlKey && e.key.toLowerCase() === "l") {
+        e.preventDefault();
+        ui.remove();
+      }
+    });
+  },
+});
