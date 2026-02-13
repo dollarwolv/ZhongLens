ece8ae5deea651cab6c2f1e22c756d7e281b4fc4
dollarwolv <dollarwolv@gmail.com>
2026-02-13 15:23:44 +0800

    Edit icons and config to display correct name

--- wxt-extension/wxt.config.ts
+++ wxt-extension/wxt.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "ZhongLens",
+    permissions: ["tabs", "activeTab"],
+  },
   vite: () => ({
     plugins: [tailwindcss()],
   }),
