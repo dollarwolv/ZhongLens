cb71dc30179623e4c916184487ca1fc4bf8df345
dollarwolv <dollarwolv@gmail.com>
2026-02-23 14:52:18 +0800

    Move background file to background/index.js

--- wxt-extension/entrypoints/background/index.js
+++ wxt-extension/entrypoints/background/index.js
@@ -1,5 +1,6 @@
 import { onMessage } from "webext-bridge/background";
 import "webext-bridge/background";
+// import { createClient } from "@supabase/supabase-js";
 
 export default defineBackground(() => {
   chrome.runtime.onInstalled.addListener(() => {
@@ -16,9 +17,44 @@ export default defineBackground(() => {
     };
 
     chrome.storage.sync.set(defaultSettings);
-
     console.log("Service worker installed");
   });
+
+  //   const chromeStorageAdapter = {
+  //     getItem: async (key) => {
+  //       return new Promise((resolve) => {
+  //         // Use chrome.storage.local.get to retrieve the item
+  //         chrome.storage.local.get([key], (result) => {
+  //           resolve(result[key] ?? null);
+  //         });
+  //       });
+  //     },
+  //     setItem: async (key, value) => {
+  //       return new Promise((resolve, reject) => {
+  //         // Use chrome.storage.local.set to store the item
+  //         chrome.storage.local.set({ [key]: value }, resolve);
+  //       });
+  //     },
+  //     removeItem: async (key) => {
+  //       return new Promise((resolve) => {
+  //         // Use chrome.storage.local.remove to remove the item
+  //         chrome.storage.local.remove([key], resolve);
+  //       });
+  //     },
+  //   };
+
+  //   const supabase = createClient(
+  //     import.meta.env.VITE_SUPABASE_URL,
+  //     import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+  //     {
+  //       auth: {
+  //         storage: chromeStorageAdapter, // Use the custom adapter
+  //         persistSession: true,
+  //         autoRefreshToken: true,
+  //         detectSessionInUrl: true,
+  //       },
+  //     },
+  //   );
 
   async function ensureOffscreen() {
     if (chrome.runtime.getContexts) {
