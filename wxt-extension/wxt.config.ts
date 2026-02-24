12f17bf2a8fb70e4db7024742ab08360a4d066a5
dollarwolv <dollarwolv@gmail.com>
2026-02-24 17:46:41 +0800

    Initialize next.js website for marketing and handling auth redirects etc.

--- wxt-extension/wxt.config.ts
+++ wxt-extension/wxt.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   manifest: {
     name: "ZhongLens",
     permissions: ["tabs", "activeTab", "storage", "offscreen"],
+    version: "0.0.1",
+    key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5QX0bnk8nrI56AcRAVQhbDH53JESy9vamUSsEPcLhoVOwuKbyy2PUUDU6SYWB17e4WK6rB6zvs97LuuaG1sAWSak+DpMR1hOINjHe+bU7zS0ScYJXFuktC4iO4H5zIvhQJyokXo9/o/soAHBakdb7zC4TynO1CuVRzaI9NB671U7pTSHsDikXqheQE8xoCRQ4b2Gjd9s4ufNQOo7yh9gal+W+HEXYEDfH6ovj1egYMncZVIMc2mQ0fSg5EmkTY5W/NEEf5RDL6x4+9UpAr4kwu/J+RZiBYEYoS22jBo7LvsdTmDNuR4axwWqqO9Dq0eeTfLATYjOiWWk8mWgdi/h2QIDAQAB",
     host_permissions: ["<all_urls>"],
     content_security_policy: {
       extension_pages:
