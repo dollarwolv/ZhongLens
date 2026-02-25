194ef07376da11e5612406673f30af7d8480fdb7
dollarwolv <dollarwolv@gmail.com>
2026-02-25 21:31:32 +0800

    Set up email verification (without sending an email)

---
+++ website/zhonglens/app/api/unsubscribe/route.js
@@ -0,0 +1,26 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  try {
+    const supabase = await createClient();
+
+    const { searchParams } = new URL(req.url);
+    const unsubscribe_token = searchParams.get("unsubscribe_token");
+
+    if (!unsubscribe_token) throw new Error("Missing unsubscribe token");
+
+    const { error } = await supabase
+      .from("waitlist_users")
+      .update({
+        subscribed_status: false,
+        unsubscribed_at: new Date().toISOString(),
+      })
+      .eq("unsubscribe_token", unsubscribe_token);
+
+    if (error) throw error;
+    return NextResponse.redirect(new URL("/unsubscribe", req.url));
+  } catch (error) {
+    return NextResponse.json({ ok: false, error: error.message });
+  }
+}
---
+++ website/zhonglens/app/unsubscribe/page.jsx
@@ -0,0 +1,12 @@
+function Unsubscribe() {
+  return (
+    <div className="w-screen flex justify-center">
+      <span className="text-2xl font-bold mt-12">
+        You have successfully unsubscribed from the ZhongLens newsletter. You
+        may now close this page.
+      </span>
+    </div>
+  );
+}
+
+export default Unsubscribe;
